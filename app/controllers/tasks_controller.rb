725c0866d17459c2acf219a94e47ffbe9a1e7dee
Kristina Sananikone <snk.kristina@gmail.com>
2021-06-08 16:02:59 +0200

    exo done and cleared, completed check to do

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
     # @task = Task.new
@@ -22,7 +24,25 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
+
+  def set_task
+    id = params[:id]
+    @task = Task.find(id)
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
