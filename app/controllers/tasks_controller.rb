633a3b84ae6209b7775d06e44c38124193d23356
Kristina Sananikone <snk.kristina@gmail.com>
2021-06-08 14:46:46 +0200

    Create and new done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,24 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+
+    if task.save
+      redirect_to task_path(task)
+    else
+      raise
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
